ebdf9f9f7fa7f6ebb427b0f34e1d0c98754195da
Mitali2700 <Mitali2700>
2022-12-03 12:52:19 +0530

    variable.js and variable-to-promise.js

--- variable-to-promise.js
+++ variable-to-promise.js
@@ -1,24 +1,30 @@
 // 2022-12-03
 
-let x;
-function change1(callback){
-    setTimeout(()=>{
-        x=1; // Setting x as 1 asynchronously.
-        callback(x);
-    },1000);
+// let x;
+async function change1(){
+    let promise= new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            x=1; // Setting x as 1 asynchronously.
+            resolve();
+        },1000);
+    })
+    let result = await promise;
 }
 
 //x=4;
 
 function change2(){
-    x=x+1;  //x=2
-    console.log(x);
+     x=x+1;  //x=2
+     console.log(x);
 }
 
-change1(callback)
-.then((x)=>{
+change1()
+.then(()=>{
     change2();
 })
-.catch((x)=>{
-    console.log("Error");
+.then((param)=>{
+    console.log(param);
 })
+
+//Promise chaining 
+
